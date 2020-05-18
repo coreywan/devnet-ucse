024d195ac152e333b7044f568c9e4d2d9735f28f
Corey Wanless <coreywanless@gmail.com>
2020-05-17 21:19:23 -0500

    example convert xml file

---
+++ convertxmlfile.py
@@ -0,0 +1,7 @@
+from ucsmsdk.utils.converttopython import convert_to_ucs_python
+
+def main():
+    convert_to_ucs_python(xml=True, literal_path="C:\\Users\\Administrator\\Downloads\\test01-serviceprofiletemplate_xmlReq.log")
+
+if __name__ == "__main__":
+    main()
